729ca1f1ad44c071db685e0c5afc86c5cd9ce320
陈月培 <1943265180@qq.com>
2020-05-02 22:59:35 +0800

    完成了button组件

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,65 @@
 <template>
   <div id="app">
-    itcast-ui
+      <div class="row">
+      <hm-button>按钮</hm-button>
+      <hm-button type="primary">按钮</hm-button>
+      <hm-button type="success">按钮</hm-button>
+      <hm-button type="info">按钮</hm-button>
+      <hm-button type="warning">按钮</hm-button>
+      <hm-button type="danger">按钮</hm-button>
+    </div>
+    <div class="row">
+      <hm-button plain>按钮</hm-button>
+      <hm-button plain type="primary">按钮</hm-button>
+      <hm-button plain type="success">按钮</hm-button>
+      <hm-button plain type="info">按钮</hm-button>
+      <hm-button plain type="warning">按钮</hm-button>
+      <hm-button plain type="danger">按钮</hm-button>
+    </div>
+    <div class="row">
+      <hm-button round>按钮</hm-button>
+      <hm-button round icon="edit" type="primary">按钮</hm-button>
+      <hm-button round type="success">按钮</hm-button>
+      <hm-button round type="info">按钮</hm-button>
+      <hm-button round type="warning">按钮</hm-button>
+      <hm-button round type="danger">按钮</hm-button>
+    </div>
+    <div class="row">
+      <hm-button icon="search" circle></hm-button>
+      <hm-button icon="edit" circle type="primary"></hm-button>
+      <hm-button icon="check" circle type="success"></hm-button>
+      <hm-button icon="message" circle type="info"></hm-button>
+      <hm-button icon="delete" circle type="warning"></hm-button>
+      <hm-button icon="delete" circle type="danger"></hm-button>
+    </div>
+    <div class="row">
+      <hm-button disabled>按钮</hm-button>
+      <hm-button disabled type="primary">按钮</hm-button>
+      <hm-button disabled type="success">按钮</hm-button>
+      <hm-button disabled type="info">按钮</hm-button>
+      <hm-button disabled type="warning">按钮</hm-button>
+      <hm-button disabled type="danger">按钮</hm-button>
+    </div>
   </div>
 </template>
 
 <script>
-
+export default {
+    methods: {
+    getInfo () {
+      console.log('获取信息！！')//获取信息！！
+    }
+  }
+}
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.row {
+ margin-bottom: 20px;
+ .hm-button{
+   margin-right: 20px;
+ }
+}
 
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import HmButton from './components/button.vue'
 Vue.config.productionTip = false
+import './assets/fonts/font.scss'
+Vue.component(HmButton.name,HmButton)
 
 new Vue({
   render: h => h(App),
